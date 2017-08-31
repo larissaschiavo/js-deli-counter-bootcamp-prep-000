8779b7e0d30b9c0ba9a61f9fe6ecbb0941ed66af
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 21:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, personName) {
   if katzDeliLine.length === 0 {
     katzDeliLine[0] = personName
+    return 1
   } else {
     katzDeliLine.push(personName)
+    return katzDeliLine.length 
   }
 }
