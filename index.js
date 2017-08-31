27f6278992f5176520b27876b8249a21379ede61
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 22:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!'
   } else {
-    const n = katzDeliLine[0]
+    const n = katzDeliLine[0];
     delete katzDeliLine[0];
     return `Currently serving ${n}.`
 
