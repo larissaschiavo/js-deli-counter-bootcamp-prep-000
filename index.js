dcd41878c99506b0a3bf2fa4d9f5b667fef74919
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 21:43:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function takeANumber(katzDeliLine, personName) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "The line is empty."
+    return 'There is nobody waiting to be served!'
   } else {
     return `Currently serving ${katzDeliLine[0]}.`
     katzDeliLine.unshift();
