0165680a9e981f28e8495f1cf50de9f8be7c4f14
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 22:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ function currentLine(line) {
   if(line.length === 0){
     return "The line is currently empty."
   } else {
-    for(let i = 0; i < line.length; i ++)
+    for(let i = 0; i < line.length; i ++) {
+      ans.pop(`line[i]``, )
+    }
   }
 }
