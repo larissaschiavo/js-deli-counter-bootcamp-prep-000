72f4bc9e1a01556787d71cd80f0774cf21e54c1e
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 21:37:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, personName) {
   if (katzDeliLine.length === 0) {
     katzDeliLine[0] = personName
-    return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
+    // return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
   } else {
     katzDeliLine.push(personName)
-    return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
+    // return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
   }
+    return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
 }
